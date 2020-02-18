628bb1931eda071b5caa21e76f2ff3190c0eb625
Jack <pupeljack@gmail.com>
2020-02-17 18:49:40 -0600

    testing more

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer: React.FC = () => {
   return (
     <div className="footer">
-      <span>CLUB PENGUIN 2 &reg; {new Date().getFullYear()}</span>
+      <span>CLUB PENGUIN 2!! &reg; {new Date().getFullYear()}</span>
     </div>
   );
 };
